fd06696a30d0f2f9f696fff832c2f00423f220ad
Daniel Santos <daniel@codein.ca>
2020-06-09 02:49:22 -0400

    attempt to move unsplash api to a proxy cloud function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,33 @@
 import * as functions from 'firebase-functions';
+const got = require('got');
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!');
-});
+export const helloWorld = functions.https.onRequest(
+  async (request, response) => {
+    const x = await getImageResponse();
+    console.log('FUNCTION RESPONSE', x);
+    response.send(x);
+  }
+);
+
+// const API_SECRET = 'OgusaG3C7qC_qI9fGc8dhoiP4b7N-dfMU4RhGFCqZZw';
+const YOUR_ACCESS_KEY = 'HvSjTTedGKj9J1zsWJbZzvOtZw4Gvj6epnxOCvBf9Kw';
+const API_URL =
+  'https://api.unsplash.com/photos/random?orientation=landscape&query=nature&featured>';
+const getImageResponse = async () => {
+  const { body } = await got.get(API_URL, {
+    headers: {
+      Authorization: 'Client-ID ' + YOUR_ACCESS_KEY,
+    },
+  });
+  return body as ImageResponse;
+};
+
+export type ImageResponse = {
+  user: { name: string; links: { html: string } };
+  urls: { small: string; regular: string; full: string };
+};
+
+// export { getImageResponse };
