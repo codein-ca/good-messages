4bb01a6733672fc10f95a7defb89bf33121b755e
Daniel Santos <daniel@codein.ca>
2020-06-09 02:47:59 -0400

    deploys a simpler ui, w/ no background image

--- src/components/API_UNSPLASH.ts
+++ src/components/API_UNSPLASH.ts
@@ -1,24 +1,20 @@
-// const API_SECRET = 'OgusaG3C7qC_qI9fGc8dhoiP4b7N-dfMU4RhGFCqZZw';
-const YOUR_ACCESS_KEY = 'HvSjTTedGKj9J1zsWJbZzvOtZw4Gvj6epnxOCvBf9Kw';
-const API_URL = 'https://api.unsplash.com/photos/random?orientation=landscape&query=nature&featured>';
+const API_URL =
+  'https://us-central1-daily-reminder-ca.cloudfunctions.net/helloWorld';
 const getImageResponse = async () => {
-    const response = await fetch(API_URL, {
-            headers: {
-                Authorization: 'Client-ID ' + YOUR_ACCESS_KEY
-            },
-        }
-    ).then(a => {
-        return a.json();
-    }).then((b: ImageResponse) => {
-        console.log(b)
-        return b
+  const response = await fetch(API_URL)
+    .then((a) => {
+      return a.json();
+    })
+    .then((b: ImageResponse) => {
+      console.log(b);
+      return b;
     });
-    return response;
-}
+  return response;
+};
 
 export type ImageResponse = {
-    user: { name: string, links: { html: string } }, urls: { small: string, regular: string, full: string }
-}
+  user: { name: string; links: { html: string } };
+  urls: { small: string; regular: string; full: string };
+};
 
-
-export {getImageResponse};
+export { getImageResponse };
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,50 +1,50 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import {getVerseOfTheDay} from "./API_YOUVERSION";
-import {getImageResponse, ImageResponse} from "./API_UNSPLASH";
-
+import { getVerseOfTheDay } from './API_YOUVERSION';
+// import { getImageResponse, ImageResponse } from './API_UNSPLASH';
 
 function App() {
-    const date = new Date(Date.now()).toDateString();
-    const [passage, setPassage] = useState<string>('');
-    const [imageResponse, setImageResponse] = useState<ImageResponse>({
-        user: {name: '', links: {html: ''}},
-        urls: {regular: '', full: '', small: ''}
-    });
-    useEffect(() => {
-        getVerseOfTheDay().then((p) => setPassage(p));
-        getImageResponse().then((i) => setImageResponse(i));
-    }, []);
+  const date = new Date(Date.now()).toDateString();
+  const [passage, setPassage] = useState<string>('');
+  // const [imageResponse, setImageResponse] = useState<ImageResponse>({
+  //   user: { name: '', links: { html: '' } },
+  //   urls: { regular: '', full: '', small: '' },
+  // });
+  useEffect(() => {
+    getVerseOfTheDay().then((p) => setPassage(p));
+    // getImageResponse().then((i) => setImageResponse(i));
+  }, []);
 
-    const unsplashImage = {
-        backgroundImage: 'url(' + imageResponse.urls.regular + ')',
-        minHeight: '100vh',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        display: 'flex',
-        flexDirection: 'column' as 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-    };
+  // const unsplashImage = {
+  //   backgroundImage: 'url(' + imageResponse.urls.regular + ')',
+  //   minHeight: '100vh',
+  //   backgroundPosition: 'center',
+  //   backgroundRepeat: 'no-repeat',
+  //   backgroundSize: 'cover',
+  //   display: 'flex',
+  //   flexDirection: 'column' as 'column',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // };
 
-
-    return (
-        <div style={unsplashImage}>
-            <header className="App-header">
-                <h1 className="App-title">Today is {`${date}`}</h1>
-            </header>
-            <main className="App-main">
-                <p>{passage}</p>
-            </main>
-            <footer>
-                {/*TODO: double-check imageResponse types*/}
-                <h2 className="App-footer">Photo by <a
-                    href={imageResponse.user.links.html}>{imageResponse.user.name}</a> from <a
-                    href="https://unsplash.com">Unsplash</a></h2>
-            </footer>
-        </div>
-    );
+  return (
+    <div className="App-root">
+      {/*<aside style={unsplashImage} />*/}
+      <header className="App-header">
+        <span className="App-title-date">{`${date}`}</span>
+      </header>
+      <main className="App-main">
+        <h1 className="App-passage">{passage}</h1>
+      </main>
+      {/*<footer>*/}
+      {/*  <h2 className="App-footer">*/}
+      {/*    Photo by{' '}*/}
+      {/*    <a className="App-author-link" href={imageResponse.user.links.html}>{imageResponse.user.name}</a>{' '}*/}
+      {/*    from <a href="https://unsplash.com">Unsplash</a>*/}
+      {/*  </h2>*/}
+      {/*</footer>*/}
+    </div>
+  );
 }
 
 export default App;
